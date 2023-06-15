740d3d04131021d14c310eed5a82e659165eb22b
bhaumik-2206 <131759257+bhaumik-2206@users.noreply.github.com>
2023-06-15 18:56:51 +0530

    Today

---
+++ 15 June/index.js
@@ -0,0 +1,36 @@
+// let x = 20;
+
+// function abc() {
+//     console.log(x);
+// }
+
+
+// if(true){
+//     a=10;
+// }
+// console.log(a);
+// var a;
+
+// function first() {
+//     second();
+//     console.log("This is First Function");
+// }
+
+// function second() {
+//     third();
+//     console.log("This is second Function");
+// }
+
+// function third() {
+//     console.log("This is third Function");
+// }
+
+// first();
+
+
+
+third();
+
+function third()  {
+    console.log("This is third Function");
+}
