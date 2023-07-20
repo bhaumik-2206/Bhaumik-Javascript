9f171440ad96a0ed66012febc57ee19540d31051
bhaumik-2206 <131759257+bhaumik-2206@users.noreply.github.com>
2023-07-20 11:54:19 +0530

    Till 11:30

---
+++ 20 July/script.js
@@ -0,0 +1,63 @@
+// document.getElementById('mouseOver').addEventListener('mouseover', function () {
+//     console.log("Mouse Over");
+// });
+
+// document.getElementById('mouseMove').addEventListener('mousemove', function () {
+//     console.log("Mouse Move");
+// });
+
+// document.getElementById('mouseEnter').addEventListener('mouseup', function () {
+//     console.log("Mouse Enter");
+// });
+
+// document.addEventListener('keydown', function (e) {
+//     if (e.key == 'Enter') {
+//         console.log("Hello");
+//     }else{
+//         console.log("You entered another key ");
+//     }
+// })
+
+// function keyDown(a) {
+//     console.log('Keydown event:', a.key);
+// }
+// function KeyUp(a) {
+//     console.log('Keyup event:', a.key);
+// }
+// function KeyPress(a) {
+//     console.log('Keypress event:', a.key);
+// }
+// document.addEventListener('keydown', keyDown);
+// document.addEventListener('keyup', KeyUp);
+// document.addEventListener('keypress', KeyPress);
+
+
+// Difference Between mouseover and mouseenter / mouseout and mouseleave
+// document.getElementById('mouseover').addEventListener('mouseover', function () {
+//     console.log("Mouse Over");
+// });
+// document.getElementById('mouseenter').addEventListener('mouseenter', function () {
+//     console.log("Mouse Enter");
+// });
+// document.getElementById('mouseout').addEventListener('mouseout', function () {
+//     console.log("Mouse Out");
+// });
+// document.getElementById('mouseleave').addEventListener('mouseleave', function () {
+//     console.log("Mouse Leave");
+// });
+
+
+// document.getElementById('innertext').innerText = "Hello World";
+// document.getElementById('innerHTML').innerHTML = "Hello World";
+// document.getElementById('textContent').textContent = "Hello World";
+
+// let a = document.getElementById('innertext');
+// let b = document.getElementById('innerHTML');
+// let c = document.getElementById('textContent');
+// console.log(a.innerText);
+// console.log(b.innerHTML);
+// console.log(c.textContent);
+
+
+
+
