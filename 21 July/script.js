2c528348482f5202ddcdad1a6b169048a262f797
bhaumik-2206 <131759257+bhaumik-2206@users.noreply.github.com>
2023-07-21 13:12:13 +0530

    Till 1:00

---
+++ 21 July/script.js
@@ -0,0 +1,32 @@
+// function addEvent() {
+//     let para = document.getElementById('addPaddingLeft');
+//     let cur = parseInt(getComputedStyle(para).paddingLeft, 10);
+//     let newPadding = cur + 10;
+//     para.style.paddingLeft = newPadding + "px";
+// }
+// setInterval(addEvent, 1000);
+
+// const intervalID = setInterval(myCallback, 1000, "Parameter 1", "Parameter 2");
+// function myCallback(a, b) {
+//     console.log(a);
+//     console.log(b);
+// }
+
+// let span = document.getElementById('span');
+// setInterval(function () {
+//     let date = new Date();
+//     span.innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+// }, 1000);
+
+// let paragraph = document.getElementById('para');
+// paragraph.addEventListener('click', function (e) {
+//     console.log(e);
+// });
+// document.addEventListener('keyup', function (e) {
+//     console.log(e);
+// });
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(i + " - " + String.fromCharCode(i));
+// }
+
