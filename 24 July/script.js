c9282fd56bd015a23ed005ebc0d692c563e98207
bhaumik-2206 <131759257+bhaumik-2206@users.noreply.github.com>
2023-07-24 13:12:19 +0530

    Till 1:00

--- 24 July/script.js
+++ 24 July/script.js
@@ -21,3 +21,43 @@ image.forEach(value => {
     });
 });
 
+
+// Notification
+let notification = document.getElementById('noti');
+let mainNoti = document.getElementById('mainNoti');
+let diffNoti = document.querySelectorAll('.diff-noti');
+function randomNotification() {
+    let newElement = document.createElement('div');
+    newElement.innerHTML = diffNoti[Math.floor(Math.random() * diffNoti.length)].innerHTML;
+    newElement.style.display = "block";
+    mainNoti.appendChild(newElement);
+}
+setInterval(() => {
+    if (!(mainNoti.children[0] == undefined)) {
+        mainNoti.children[0].remove();
+    }
+}, 1000);
+notification.addEventListener('click', randomNotification);
+
+
+
+let allTr = document.querySelectorAll('tr');
+let newArray = [];
+for (let i = 1; i < allTr.length; i++) {
+    let a = allTr[0].children;
+    let newObject = {};
+    for (let j = 0; j < a.length; j++) {
+        let id = a[j].innerHTML;
+        let value = allTr[i].children[j].innerHTML;
+        newObject[id] = value;
+    }
+    newArray.push(newObject);
+}
+document.getElementById('pre').innerHTML = newArray;
+console.log(newArray);
+
+
+let clicking = document.getElementById('clicking');
+clicking.addEventListener('click', function (e) {
+    console.log(e.offsetX);
+});
