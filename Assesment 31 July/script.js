25daff7e2d7e2c64af242f397cd5de018ed4f1b2
bhaumik-2206 <131759257+bhaumik-2206@users.noreply.github.com>
2023-07-31 19:10:12 +0530

    Till 7:10

--- Assesment 31 July/script.js
+++ Assesment 31 July/script.js
@@ -5,6 +5,8 @@ let getAmount = document.getElementById('getAmount');
 let total = document.getElementById('total');
 let total1 = document.getElementById('total1');
 let form = document.getElementById('form');
+let green = document.getElementById('green');
+let red = document.getElementById('red');
 
 amount.addEventListener('blur', function () {
     let price = Number(amount.value);
@@ -21,29 +23,58 @@ function calculateDiscount() {
     let priceAfterGST = (Number(amount.value) + (amount.value * 18) / 100);
     if (amount.value >= 500 && amount.value < 2000) {
         if (discount == 'off25') {
+            displayApplied();
             total.innerHTML = `<span>(-25%)</span> ${priceAfterGST - (priceAfterGST * 25) / 100}`;
-            total1.innerHTML = priceAfterGST - (priceAfterGST * 25) / 100;
+            total1.innerHTML = Math.round(priceAfterGST - (priceAfterGST * 25) / 100);
+        } else {
+            red.style.display = "block";
+            red.innerHTML = "Please Try OFF 25 coupon";
+            green.style.display = "none";
         }
     } else if (amount.value >= 2000 && amount.value <= 4000) {
         if (discount == 'off25') {
+            displayApplied();
             total.innerHTML = `<span>(-25%)</span> ${priceAfterGST - (priceAfterGST * 25) / 100}`;
-            total1.innerHTML = priceAfterGST - (priceAfterGST * 25) / 100;
+            total1.innerHTML = Math.round(priceAfterGST - (priceAfterGST * 25) / 100);
         } else if (discount == 'off50') {
+            displayApplied();
             total.innerHTML = `<span>(-50%)</span> ${priceAfterGST - (priceAfterGST * 50) / 100}`;
-            total1.innerHTML = priceAfterGST - (priceAfterGST * 50) / 100;
+            total1.innerHTML = Math.round(priceAfterGST - (priceAfterGST * 50) / 100);
+        } else {
+            red.style.display = "block";
+            red.innerHTML = "Please Try OFF 25 and OFF 50 coupon";
+            green.style.display = "none";
         }
     } else if (amount.value > 4000) {
         if (discount == 'off25') {
+            displayApplied();
             total.innerHTML = `<span>(-25%)</span> ${priceAfterGST - (priceAfterGST * 25) / 100}`;
-            total1.innerHTML = priceAfterGST - (priceAfterGST * 25) / 100;
+            total1.innerHTML = Math.round(priceAfterGST - (priceAfterGST * 25) / 100);
         } else if (discount == 'off50') {
+            displayApplied();
             total.innerHTML = `<span>(-50%)</span> ${priceAfterGST - (priceAfterGST * 50) / 100}`;
-            total1.innerHTML = priceAfterGST - (priceAfterGST * 50) / 100;
+            total1.innerHTML = Math.round(priceAfterGST - (priceAfterGST * 50) / 100);
         } else if (discount == 'off75') {
+            displayApplied();
             total.innerHTML = `<span>(-75%)</span> ${priceAfterGST - (priceAfterGST * 75) / 100}`;
-            total1.innerHTML = priceAfterGST - (priceAfterGST * 75) / 100;
+            total1.innerHTML = Math.round(priceAfterGST - (priceAfterGST * 75) / 100);
+        } else {
+            red.style.display = "block";
+            red.innerHTML = "Your coupon is Not Working";
+            green.style.display = "none";
         }
+    } else {
+        red.style.display = "block";
+        red.innerHTML = "Please buy more than 500 RS to use the coupon";
+        green.style.display = "none";
     }
+    // console.log(total.textContent.slice(7));
+    // total.innerHTML = Number(total.textContent.slice(7)).toPrecision(4);
+}
+function displayApplied() {
+    green.style.display = "block";
+    green.innerHTML = "Offer Applied";
+    red.style.display = "none";
 }
 // form.addEventListener('focus', function () {
 //     check.disabled = false;
