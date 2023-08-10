85ba4eda6805442e6696ec908f5e717f060d8d7a
bhaumik-2206 <131759257+bhaumik-2206@users.noreply.github.com>
2023-08-10 19:13:33 +0530

    Till 7:15

--- 10 August/Medicine.js
+++ 10 August/Medicine.js
@@ -80,38 +80,40 @@ let table = document.getElementById('table');
 let medicineText = document.getElementById('medicine-text');
 let quantity = document.getElementById('quantity');
 let noti = document.getElementById('noti');
-let close = document.getElementById('close');
 let trueDate = false;
+let storeVar = null;
+let allMedicine = [];
 
-// For block the display of form 
-mainBtn.addEventListener('click', () => {
-    form.style.display = 'block';
-    table.style.opacity = ".5";
-    mainBtn.style.opacity = ".5";
-});
+// For block the display of form and change the opacity of button and table in click
+mainBtn.addEventListener('click', changeBackground);
 
 // Form submit event
 form.addEventListener('submit', (e) => {
-    let allMedicine = [];
-    let cell = document.querySelectorAll('.addMedicine')
-    cell.forEach(e => allMedicine.push(e.children[0].innerText));
     e.preventDefault();
+    // let cell = document.querySelectorAll('.addMedicine');
+    // cell.forEach(e => allMedicine.push(e.children[0].innerText));
     if (medicineText.value.trim() !== '' && !allMedicine.includes(medicineText.value.trim().toLowerCase())) {
         trueDate = false;
         returnExpiryDate();
         if (trueDate) {
-            let newTr = document.createElement('tr');
-            newTr.setAttribute('class', 'addMedicine');
-            newTr.innerHTML = `
-            <td>${medicineText.value}</td>
-            <td>${quantity.value}</td>
-            <td>${returnExpiryDate()}</td>
-            <td>${packDropdown.value}</td>
-            <td>${batchInput.value}</td>
-            <td><span onclick="editTask(this)">edit</span> <span onclick="deleteMedicine(this)">Delete</span></td>`;
-            table.appendChild(newTr);
-            normalBackground();
-            showNotification("Medicine Added");
+            if (storeVar) {
+                updateRow(storeVar);
+                storeVar = null;
+            } else {
+                let newTr = document.createElement('tr');
+                newTr.setAttribute('class', 'addMedicine');
+                newTr.innerHTML = `
+                <td>${medicineText.value}</td>
+                <td>${quantity.value}</td>
+                <td>${returnExpiryDate()}</td>
+                <td>${packDropdown.value}</td>
+                <td>${batchInput.value}</td>
+                <td><span onclick="editMedicine(this)">edit</span> <span onclick="deleteMedicine(this)">Delete</span></td>`;
+                table.appendChild(newTr);
+                // allMedicine.push(medicineText.value.trim().toLowerCase());
+                normalBackground();
+                showNotification("Medicine Added");
+            }
             resetAllTheValue();
         }
     } else {
@@ -143,9 +145,14 @@ function resetAllTheValue() {
 // Delete Medicine
 function deleteMedicine(e) {
     let a = confirm("Are You Sure To Delete Data?");
+    let row = e.parentElement.parentElement;
     if (a) {
-        let row = e.parentElement.parentElement;
         row.remove();
+        showNotification("Medicine Delete");
+    }
+    const index = allMedicine.indexOf(row.children[0].innerText.toLowerCase().trim());
+    if (index > -1) {
+        allMedicine.splice(index, 1);
     }
 }
 
@@ -157,16 +164,24 @@ function returnExpiryDate() {
     let selecteDate = new Date(newDate.value);
 
     if (byDays.checked) {
-        let date = new Date(selecteDate.getTime() + (allDays.value * 24 * 60 * 60 * 1000));
-        if (date.getTime() >= presentDate.getTime()) {
+        let newDate = new Date(selecteDate.getTime() + (allDays.value * 24 * 60 * 60 * 1000));
+        if (newDate.getTime() >= presentDate.getTime()) {
             trueDate = true;
-            return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+            let month = newDate.getMonth() + 1;
+            let returnDate = newDate.getDate();
+            if (month < 10) {
+                month = '0' + month;
+            }
+            if (returnDate < 10) {
+                returnDate = '0' + returnDate;
+            }
+            return newDate.getFullYear() + '-' + month + '-' + returnDate;
         } else {
             trueDate = false;
             alert("This is not a valid date");
         }
     } else if (byDate.checked) {
-        if (selecteDate.getDate() >= presentDate.getDate()) {
+        if (selecteDate.getTime() >= presentDate.getTime()) {
             trueDate = true;
             return newDate.value;
         } else {
@@ -179,6 +194,7 @@ function returnExpiryDate() {
 // Show Notification when the table row is updated.
 function showNotification(value) {
     let newp = document.createElement('p');
+    newp.style.marginBottom = "10px";
     newp.innerHTML = value;
     noti.appendChild(newp);
     setTimeout(() => {
@@ -192,5 +208,41 @@ function normalBackground() {
     mainBtn.style.opacity = 1;
     form.style.display = 'none';
 }
+function changeBackground() {
+    table.style.opacity = ".5";
+    mainBtn.style.opacity = ".5";
+    form.style.display = 'block';
+}
 
+// Close the form 
+let close = document.getElementById('close');
 close.addEventListener('click', normalBackground);
+
+
+
+
+// Edit--------------------------------
+function editMedicine(e) {
+    changeBackground();
+    let first = e.parentElement.parentElement;
+    storeVar = first;
+    medicineText.value = first.children[0].innerHTML;
+    packDropdown.value = first.children[3].innerHTML;
+    quantity.value = first.children[1].innerHTML;
+    byDate.checked = true;
+    byDays.checked = false;
+    forbyDate();
+    document.getElementById('newDate').value = first.children[2].innerHTML;;
+    batchInput.value = first.children[4].innerHTML;
+}
+
+function updateRow(row) {
+    row.children[0].innerHTML = medicineText.value;
+    row.children[1].innerHTML = quantity.value;
+    row.children[2].innerHTML = returnExpiryDate();
+    row.children[3].innerHTML = packDropdown.value;
+    row.children[4].innerHTML = batchInput.value;
+    normalBackground();
+    resetAllTheValue();
+    showNotification("Medicine Updated");
+}
