0a39d8aec4900814a7755f744f5d3352bd830d0b
bhaumik-2206 <131759257+bhaumik-2206@users.noreply.github.com>
2023-08-10 16:52:07 +0530

    Till 4:45

--- 10 August/Medicine.js
+++ 10 August/Medicine.js
@@ -1,12 +1,12 @@
 let packs = [
-    { id: 1, value: "pack - 1" },
-    { id: 2, value: "pack - 2" },
-    { id: 3, value: "pack - 3" },
-    { id: 4, value: "pack - 4" },
-    { id: 5, value: "pack - 5" },
-    { id: 6, value: "pack - 6" },
-    { id: 7, value: "pack - 7" },
-    { id: 8, value: "pack - 8" },
+    { id: 1, value: "pack-1" },
+    { id: 2, value: "pack-2" },
+    { id: 3, value: "pack-3" },
+    { id: 4, value: "pack-4" },
+    { id: 5, value: "pack-5" },
+    { id: 6, value: "pack-6" },
+    { id: 7, value: "pack-7" },
+    { id: 8, value: "pack-8" },
 ];
 let batch = [
     { pack_id: 1, batch: "A1" },
@@ -18,59 +18,60 @@ let batch = [
     { pack_id: 7, batch: "A7" },
     { pack_id: 8, batch: "A8" },
 ];
-
 // Adding a Options to Pack Dropdown
 let packDropdown = document.getElementById('pack-dropdown');
 packs.forEach(ele => {
     let option = document.createElement('option');
-    option.value = ele.id;
+    option.value = ele.value;
     option.innerText = ele.value;
     packDropdown.appendChild(option);
 });
 
-
+// Adding a Options to batch Dropdown
 let batchInput = document.getElementById('batch-input');
 batch.forEach(ele => {
     let option = document.createElement('option');
-    option.value = ele.pack_id;
+    option.value = ele.batch;
     option.innerText = ele.batch;
     batchInput.appendChild(option);
 });
 
 
-// Create element on by date
+// Create date when click on by date
 let byDate = document.getElementById('by-date');
 let addValues = document.getElementById('addValues');
 function forbyDate() {
     addValues.innerHTML = "";
-    let byDate = document.getElementById('by-date');
     if (byDate.checked) {
         let newDate = document.createElement('input');
         newDate.setAttribute('type', 'date');
+        newDate.setAttribute('id', 'newDate');
+        newDate.setAttribute('required', '');
         addValues.appendChild(newDate);
     }
 }
 byDate.addEventListener('change', forbyDate);
 
 
-// Create element on by days
+// Create date when click on by days
 let byDays = document.getElementById('by-days');
 function forbyDays() {
     addValues.innerHTML = "";
-    let byDays = document.getElementById('by-days');
     if (byDays.checked) {
         let newDate = document.createElement('input');
         newDate.setAttribute('type', 'date');
+        newDate.setAttribute('id', 'newDate');
+        newDate.setAttribute('required', '');
         addValues.appendChild(newDate);
         let selDays = document.createElement('input');
         selDays.setAttribute('type', 'number');
+        selDays.setAttribute('id', 'allDays');
         selDays.setAttribute('min', '1');
+        selDays.setAttribute('required', '');
         addValues.appendChild(selDays);
     }
 }
 byDays.addEventListener('change', forbyDays);
-
-
 
 
 let form = document.getElementById('form');
@@ -78,44 +79,57 @@ let mainBtn = document.getElementById('mainBtn');
 let table = document.getElementById('table');
 let medicineText = document.getElementById('medicine-text');
 let quantity = document.getElementById('quantity');
-let packId = 0;
+let noti = document.getElementById('noti');
+let close = document.getElementById('close');
+let trueDate = false;
 
+// For block the display of form 
 mainBtn.addEventListener('click', () => {
     form.style.display = 'block';
-    form.style.opacity = '1';
+    table.style.opacity = ".5";
+    mainBtn.style.opacity = ".5";
 });
 
+// Form submit event
 form.addEventListener('submit', (e) => {
+    let allMedicine = [];
+    let cell = document.querySelectorAll('.addMedicine')
+    cell.forEach(e => allMedicine.push(e.children[0].innerText));
     e.preventDefault();
-    if (medicineText.value.trim() !== '') {
-        let newTr = document.createElement('tr');
-        newTr.innerHTML = `
+    if (medicineText.value.trim() !== '' && !allMedicine.includes(medicineText.value.trim().toLowerCase())) {
+        trueDate = false;
+        returnExpiryDate();
+        if (trueDate) {
+            let newTr = document.createElement('tr');
+            newTr.setAttribute('class', 'addMedicine');
+            newTr.innerHTML = `
             <td>${medicineText.value}</td>
             <td>${quantity.value}</td>
-            <td>${addValues.innerHTML}</td>
+            <td>${returnExpiryDate()}</td>
             <td>${packDropdown.value}</td>
-            <td>${batchInput.innerText}</td>
-            <td><span onclick="editTask(this)">edit</span> <span onclick="deleteTask(this)">Delete</span></td>`;
-        table.appendChild(newTr);
-        console.log(packDropdown.children);
-        form.style.display = 'none';
-        console.log(packDropdown.value);
+            <td>${batchInput.value}</td>
+            <td><span onclick="editTask(this)">edit</span> <span onclick="deleteMedicine(this)">Delete</span></td>`;
+            table.appendChild(newTr);
+            normalBackground();
+            showNotification("Medicine Added");
+            resetAllTheValue();
+        }
+    } else {
+        alert("Invalid Name");
     }
-    resetAllTheValue();
 });
 
+// add value and ebable batch input depend on pack dropdown value
 packDropdown.addEventListener("change", (e) => {
     e.preventDefault();
-    packId = packDropdown.value;
-    if (packId !== "") {
-        let pack = packs.find(ele => ele.id == packId);
-        let findBatch = batch.find(ele => ele.pack_id == pack.id);
-        batchInput.value = findBatch.pack_id;
-        batchInput.disabled = false;
-        console.log(findBatch);
-    }
+    let packId = packDropdown.value;
+    let pack = packs.find(ele => ele.value == packId);
+    let findBatch = batch.find(ele => ele.pack_id == pack.id);
+    batchInput.value = findBatch.batch;
+    batchInput.disabled = false;
 });
 
+// Reset Form
 function resetAllTheValue() {
     medicineText.value = "";
     packDropdown.value = "";
@@ -126,10 +140,57 @@ function resetAllTheValue() {
     batchInput.value = "";
 }
 
-function deleteTask(e) {
+// Delete Medicine
+function deleteMedicine(e) {
     let a = confirm("Are You Sure To Delete Data?");
     if (a) {
         let row = e.parentElement.parentElement;
         row.remove();
     }
 }
+
+// For get the date which is selected by the user
+function returnExpiryDate() {
+    let allDays = document.getElementById('allDays');
+    let newDate = document.getElementById('newDate');
+    let presentDate = new Date();
+    let selecteDate = new Date(newDate.value);
+
+    if (byDays.checked) {
+        let date = new Date(selecteDate.getTime() + (allDays.value * 24 * 60 * 60 * 1000));
+        if (date.getTime() >= presentDate.getTime()) {
+            trueDate = true;
+            return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+        } else {
+            trueDate = false;
+            alert("This is not a valid date");
+        }
+    } else if (byDate.checked) {
+        if (selecteDate.getDate() >= presentDate.getDate()) {
+            trueDate = true;
+            return newDate.value;
+        } else {
+            trueDate = false;
+            alert("Please select future date");
+        }
+    }
+}
+
+// Show Notification when the table row is updated.
+function showNotification(value) {
+    let newp = document.createElement('p');
+    newp.innerHTML = value;
+    noti.appendChild(newp);
+    setTimeout(() => {
+        newp.remove();
+    }, 4000);
+}
+
+// For remove form and change opacity of table and main button.
+function normalBackground() {
+    table.style.opacity = 1;
+    mainBtn.style.opacity = 1;
+    form.style.display = 'none';
+}
+
+close.addEventListener('click', normalBackground);
