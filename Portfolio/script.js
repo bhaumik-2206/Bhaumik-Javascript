dad69ef987dd73db183a6681622a1828264b7af8
bhaumik-2206 <131759257+bhaumik-2206@users.noreply.github.com>
2023-08-31 11:38:44 +0530

    Till 11:30

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -5,4 +5,17 @@ window.addEventListener('scroll', () => {
 
         document.getElementById('main-navBar').classList.remove('fixed-top');
     }
+
+    if (window.scrollY >= 150) {
+        document.getElementById("scroll-top").classList.add("add-button");
+    } else {
+        document.getElementById("scroll-top").classList.remove("add-button");
+    }
 });
+
+document.getElementById('scroll-top').addEventListener('click', (e) => {
+    window.scroll(0, 0);
+})
+
+let allBold = document.querySelectorAll('.changeText b');
+console.log(allBold);
